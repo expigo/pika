379252a8f1a2f93c4509e98d823de67198a893bd
Kryspin Musiol <kryspin.musiol@gmail.com>
2026-01-12 00:42:49 +0100

    fix: suppress hydration warning on body for browser extensions

--- packages/web/src/app/layout.tsx
+++ packages/web/src/app/layout.tsx
@@ -28,7 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
+      >
+        {children}
+      </body>
     </html>
   );
 }
